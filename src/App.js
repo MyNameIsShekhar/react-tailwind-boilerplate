0ac858749c114e07af9f94b01698c8f825d5edb9
Shekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2024-04-12 12:38:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
 } from "@material-tailwind/react";
 import { BellIcon, Cog6ToothIcon } from "@heroicons/react/24/solid";
  
-export function App() {
+function App() {
   return (
     <Navbar
       variant="gradient"
